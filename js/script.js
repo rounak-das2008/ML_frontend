d32433c80f993a5ad1948ade905c1a7b91381067
Rounak Das <rounakdas200813@gmail.com>
2022-10-26 19:35:14 +0530

    removed unnecessary sections and comments

--- js/script.js
+++ js/script.js
@@ -1,11 +1,8 @@
 let navbar = document.querySelector('.mip-header .navbar');
-// let searchForm = document.querySelector('.header .search-form');
 let loginForm = document.querySelector('.mip-header .login-form');
-// let contactInfo = document.querySelector('.contact-info');
 
 document.querySelector('#menu-btn').onclick = () =>{
    navbar.classList.toggle('active');
-   // searchForm.classList.remove('active');
    loginForm.classList.remove('active');
 };
 
@@ -17,9 +14,6 @@ document.querySelector('#login-btn').onclick = () =>{
 
 window.onscroll = () =>{
    navbar.classList.remove('active');
-   // searchForm.classList.remove('active');
-   // loginForm.classList.remove('active');
-   // contactInfo.classList.remove('active');
 }
 
 var swiper = new Swiper(".home-slider", {
@@ -57,64 +51,3 @@ var swiper = new Swiper(".blogs-slider", {
    },
 });
 
-var nav = $('nav');
-var line = $('<div />').addClass('line');
-
-line.appendTo(nav);
-
-var active = nav.find('.active');
-var pos = 0;
-var wid = 0;
-
-if(active.length) {
-  pos = active.position().left;
-  wid = active.width();
-  line.css({
-    left: pos,
-    width: wid
-  });
-}
-
-nav.find('ul li a').click(function(e) {
-  e.preventDefault();
-  if(!$(this).parent().hasClass('active') && !nav.hasClass('animate')) {
-    
-    nav.addClass('animate');
-
-    var _this = $(this);
-
-    nav.find('ul li').removeClass('active');
-
-    var position = _this.parent().position();
-    var width = _this.parent().width();
-
-    if(position.left >= pos) {
-      line.animate({
-        width: ((position.left - pos) + width)
-      }, 300, function() {
-        line.animate({
-          width: width,
-          left: position.left
-        }, 150, function() {
-          nav.removeClass('animate');
-        });
-        _this.parent().addClass('active');
-      });
-    } else {
-      line.animate({
-        left: position.left,
-        width: ((pos - position.left) + wid)
-      }, 300, function() {
-        line.animate({
-          width: width
-        }, 150, function() {
-          nav.removeClass('animate');
-        });
-        _this.parent().addClass('active');
-      });
-    }
-
-    pos = position.left;
-    wid = width;
-  }
-});
